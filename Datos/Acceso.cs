93acb6355b34136eb92ce6a40aaf9bdd5d978cda
juniiorgarciia <33963016+juniiorgarciia@users.noreply.github.com>
2019-06-05 21:46:24 -0600

    Junior Conex (#2)

---
+++ Datos/Acceso.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace Datos
+{
+    class Acceso
+    {
+        // Clase de acceso a datos.
+
+        #region "Variables (Clases) de Conexion"
+        private SqlConnection Conexion;
+        #endregion
+
+        //Constructor
+        public Acceso()
+        {
+            Conexion = new SqlConnection(CadenaConexion);
+        }
+        private string CadenaConexion
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["FinalTresCapas.Properties.Settings.Conectar"].ToString();
+                //@"Data Source = .\SQLEXPRESS;Initial Catalog = BD_IPD; Integrated Security = true";
+            }
+
+        }
+
+        public SqlConnection ObtenerConexion()
+        {
+            return Conexion;
+        }
+    }
+}
